cb689a6d9254047561d7f60b6f76387edc6d4347
Guilherme Bomfim <guilhermemmb@gmail.com>
2015-05-12 14:06:05 -0300

    phantomjs

--- node/init.sh
+++ node/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 echo "starting phantomjs"
-/usr/bin/phantomjs --webdriver=4444 &
+phantomjs --webdriver=4444 &
 cd youcantestNode
 echo "Starting queue consumer"
 node queue_consumer.js
--- node/start_node_container.sh
+++ node/start_node_container.sh
@@ -1,13 +1,5 @@
-echo "REMOVING OLD YCTNODE"
-docker rm -f yctnode
-docker rmi -f guilhermemmb/yctnode
-
 echo "BUILDING YCTNODE CONTAINER"
 docker build -t guilhermemmb/yctnode .
 
 echo "RUNNING YCT CONTAINER"
-docker run -d \
-    --name yctnode \
-    --link rabbit:rabbit \
-    --link mongodb:mongodb \
-    -t guilhermemmb/yctnode
+docker run -d --name yctnode --link rabbit:rabbit --link mongodb:mongodb -t guilhermemmb/yctnode
