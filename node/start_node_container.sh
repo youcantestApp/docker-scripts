0527f9f60b3f6137e866814f77aa8cc7f900905d
Ubuntu <ubuntu@ip-172-31-29-122.us-west-2.compute.internal>
2015-05-15 04:25:01 +0000

    subindo o dockerfile novo

--- node/start_node_container.sh
+++ node/start_node_container.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker rm -f yctnode
+docker rmi -f guilhermemmb/yctnode
 docker build --no-cache -t guilhermemmb/yctnode .
 docker run -d -i --name yctnode --link rabbit:rabbit --link mongodb:mongodb -t guilhermemmb/yctnode
