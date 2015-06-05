49644aeaaf55a1636d3c722c89b141063effa9cd
Guilherme Bomfim <guilhermemmb@gmail.com>
2015-06-05 14:43:24 -0300

    subindo a alteração do mongo para persistir o banco

--- mongodb/start_mongodb_container.sh
+++ mongodb/start_mongodb_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build --no-cache -t guilhermemmb/yctmongodb .
-docker run -d --name mongodb -p 27017:27017 -e MONGODB_USERNAME=admin -e MONGODB_PASSWORD=admin -e MONGODB_DBNAME=youcantest_db -t guilhermemmb/yctmongodb
+docker run -d --name mongodb -p 27017:27017 -e MONGODB_USERNAME=admin -e MONGODB_PASSWORD=admin -e MONGODB_DBNAME=youcantest_db -v /persistedData/mongodb:/data -t guilhermemmb/yctmongodb
