19b0c82262a3235381971fbb2dc88be7d9f501b4
Guilherme Bomfim <guilhermemmb@gmail.com>
2015-09-25 00:46:23 -0300

    atualizando para o novo repositorio do test-executer

--- node/start_node_container.sh
+++ node/start_node_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker rm -f yctnode1
-docker rmi -f guilhermemmb/yctnode1
-docker build --no-cache -t guilhermemmb/yctnode1 .
-docker run -d -i --name yctnode1 --add-host rabbit:52.26.16.39 --add-host mongodb:52.26.16.39 -t guilhermemmb/yctnode1
+docker rm -f yctnode
+docker rmi -f guilhermemmb/yctnode
+docker build --no-cache -t guilhermemmb/yctnode .
+docker run -d -i --name yctnode --add-host rabbit:52.26.16.39 --add-host mongodb:52.26.16.39 -t guilhermemmb/yctnode
